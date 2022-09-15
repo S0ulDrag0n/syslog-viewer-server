8cc5baa02da198eaf13b90de55eb38c2a501bf44
S0ulDrag0n <SD.S0ulDrag0n@gmail.com>
2022-09-15 00:14:27 -0700

    Added more logging

--- server.js
+++ server.js
@@ -6,22 +6,30 @@ const LogManager = require('./lib/Managers/LogManager');
 const SyslogServerManager = require('./lib/Managers/SyslogServerManager');
 const logger = require('./lib/logger');
 
+logger.log('Server', 'Instantiating syslog...');
 const syslogManager = new SyslogServerManager();
 syslogManager.start();
+logger.log('Server', 'Syslog ready!');
 
+logger.log('Server', 'Instantiating log manager...');
 const logManager = new LogManager(process.env.MYSQL_HOST, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD);
+logger.log('Server', 'Log manager ready!');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = '';
+const hostname = '0.0.0.0';
 const port = 3000;
 
+logger.log('Server', 'Instantiating server...');
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
+logger.log('Server', 'Preparing server...');
 app.prepare().then(async () => {
+  logger.log('Server', 'Starting database connection initialization...');
   await logManager.initialize();
 
+  logger.log('Server', 'Starting custom server...');
   const httpServer = createServer(async (req, res) => {
     try {
       // Be sure to pass `true` as the second argument to `url.parse`.
